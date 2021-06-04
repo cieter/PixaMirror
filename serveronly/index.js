e5d9394a10874193ac306efbc42f0e8748f7c97d
Cejay <cjvanwel@hotmail.nl>
2021-06-04 10:33:15 +0200

    Removed complete repo

--- serveronly/index.js
+++
@@ -1,8 +0,0 @@
-const app = require("../js/app.js");
-const Log = require("logger");
-
-app.start(function (config) {
-	var bindAddress = config.address ? config.address : "localhost";
-	var httpType = config.useHttps ? "https" : "http";
-	Log.log("\nReady to go! Please point your browser to: " + httpType + "://" + bindAddress + ":" + config.port);
-});
